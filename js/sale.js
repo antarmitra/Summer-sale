8e94257b4e04ccdb194a9ef88b33af5ed4592985
AntarProgrammer <antarmitra33@gmail.com>
2023-08-21 13:55:08 +0600

    complete project with responsive

--- js/sale.js
+++ js/sale.js
@@ -1,69 +1,69 @@
-// total price and name 
+// total price and select product name
+
 let total = 0;
-function clickedMe(target){
+function clickedMe(target) {
     const infoContainer = document.getElementById('info');
     const item = target.childNodes[3].childNodes[3].innerText;
     console.log(item)
     const count = infoContainer.childElementCount;
     const p = document.createElement("p");
-    p.innerText =`${count+1}. `+ item;
+    p.innerText = `${count + 1}. ` + item;
     infoContainer.appendChild(p);
 
-    const price =  target.childNodes[3].childNodes[5].innerText;
+    const price = target.childNodes[3].childNodes[5].innerText;
     total = parseFloat(total) + parseFloat(price)
     const totalPriceToFixed = total.toFixed(2)
     document.getElementById('total').innerText = totalPriceToFixed
 
 
-        // button disabled
+    // button disabled
+    const button = document.getElementById('apply-btn');
+    if (total >= 200) {
+        button.removeAttribute('disabled')
+    }
+    else {
+        button.setAttribute('disabled', true)
+    }
 
-        const button = document.getElementById('apply-btn');
-        if( total >= 200){
-            button.removeAttribute('disabled')
-        }
-        else{
-            button.setAttribute('disabled', true)
-        }
-
-        const purchaseButton = document.getElementById('purchase-btn');
-        if(total > 0){
-            purchaseButton.removeAttribute('disabled')
-        }
-        else{
-            purchaseButton.setAttribute('disabled', true)
-        }
+    const purchaseButton = document.getElementById('purchase-btn');
+    if (total > 0) {
+        purchaseButton.removeAttribute('disabled')
+    }
+    else {
+        purchaseButton.setAttribute('disabled', true)
+    }
 
 
 }
 
 
-
-    document.getElementById('apply-btn').addEventListener('click', function(){
-        const couponInput = document.getElementById('coupon')
-        
-        if(couponInput.value === 'SELL200'){
-
-            discount = (20 * total) / 100;
-            const discountField = discount.toFixed(2);
-            const discountTotal = document.getElementById('discount')
-            discountTotal.innerText = discountField;
-
-            totalPrice = total - discountField; 
-            const totalInput = totalPrice.toFixed(2);
-            const totalElement = document.getElementById('total-price');
-            totalElement.innerText = totalInput;
-        }
-        else{
-            alert('Please input valid coupon')
-        }
-
-    })
-
-
-    document.getElementById('go-home-button').addEventListener('click', function(){
-        window.location.href = 'index.html'
-    })
-  
 // discount
-    
+
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const couponInput = document.getElementById('coupon')
+
+    if (couponInput.value === 'SELL200') {
+
+        discount = (20 * total) / 100;
+        const discountField = discount.toFixed(2);
+        const discountTotal = document.getElementById('discount')
+        discountTotal.innerText = discountField;
+
+        totalPrice = total - discountField;
+        const totalInput = totalPrice.toFixed(2);
+        const totalElement = document.getElementById('total-price');
+        totalElement.innerText = totalInput;
+    }
+    else {
+        alert('Please input valid coupon')
+    }
+
+})
+
+// modal section
+document.getElementById('go-home-button').addEventListener('click', function () {
+    window.location.href = 'index.html'
+})
+
+
 
